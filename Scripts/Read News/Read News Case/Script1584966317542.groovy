1dcf747c98fbee1b4ad4aad4875e1b08b5c1f9c1
didimuscandra <didimuscandra@gmail.com>
2020-03-23 20:31:24 +0700

    lagiii

--- Scripts/Read News/Read News Case/Script1584966317542.groovy
+++ Scripts/Read News/Read News Case/Script1584966317542.groovy
@@ -15,6 +15,8 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
 
 WebUI.openBrowser('')
 
@@ -23,10 +25,13 @@ WebUI.navigateToUrl('http://localhost/CodeIgniterCRUD/index.php/news/')
 WebDriver driver = DriverFactory.getWebDriver()
 
 WebElement Table = driver.findElement(By.xpath('(.//tbody//child::tr[2]//child::td)[1]'))
-
-WebElement Table2 = driver.findElement(By.xpath('(.//tbody//child::tr[2]//child::td)[2]'))
+String title = Table.getText()
 
 WebUI.click(findTestObject('Object Repository/Page_CodeIgniter Tutorial/a_view'))
+WebElement DivTitle = driver.findElement(By.xpath('.//h2'))
+String title2 = DivTitle.getText()
+
+WebUI.verifyEqual(title,title2)
 
 WebUI.closeBrowser()
 
