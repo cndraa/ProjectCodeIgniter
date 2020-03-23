c59f1ee0adbc3f217ca5ae8a387afef875a4bc7c
didimuscandra <didimuscandra@gmail.com>
2020-03-23 16:40:22 +0700

    FirstCommit

---
+++ Object Repository/Page_CodeIgniter Tutorial/body_Simple CRUDHome  Add NewsCreate a news_c9cba1.rs
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_Simple CRUDHome  Add NewsCreate a news_c9cba1</name>
+   <tag></tag>
+   <elementGuidId>4ce97571-2c3a-4170-885b-fda176faa5de</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>body</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+		Simple CRUD
+		Home | Add News
+Create a news item
+ 
+ 
+
+    
+    
+        
+            Title
+            
+        
+        
+            Text
+            
+        
+        
+            
+            
+        
+        
+
+Copyright © 2016
+
+
+/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body</value>
+   </webElementXpaths>
+</WebElementEntity>
